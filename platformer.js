9c351f080c0c809373ec37dc3b419762f8ce5515
Chandrakant <ckdhruw007@gmail.com>
2017-09-05 20:27:54 +0530

    Add files via upload

--- platformer.js
+++ platformer.js
@@ -6,8 +6,8 @@ holdleft = holdright = paused = false;
 onG = false;
 plats = [];
 obstacles = [];
-framesPerSecond = 40;
-var platform, plat1, plat2, platform2, intrVal;
+framesPerSecond = 60;
+var plat, platform, plat1, plat2, platform2, intrVal;
 
 window.onload = function () {
     canv = document.getElementById('cv');
@@ -22,16 +22,14 @@ window.onload = function () {
 
 function update() {
     frameNo++;
+    background.update();
     if (holdleft) {
         xv = -3;
     }
     if (holdright) {
         xv = 3;
     }
-    //player.x += xv;
-    platform.x -= xv;
-    platform2.x -= xv;
-    platform3.x -= xv;
+
 
     player.y += yv;
 
@@ -46,36 +44,53 @@ function update() {
 
     // check whether player is in a platform
     for (i = 0; i < plats.length; i++) {
-        if ((player.x+player.width > plats[i].x) && 
-            (player.x < plats[i].x + plats[i].width) && 
-            (player.y > plats[i].y ) &&  // && player.y <= plats[i].y + player.width
-            (player.y + player.width < plats[i].y + plats[i].height)) { //
-            player.y = plats[i].y;
-            player.angle = 0;                                       // To land player on ground without any roation angle
-            onG = true;
+        if ((player.x + player.width > plats[i].x) &&
+            (player.x < plats[i].x + plats[i].width)) {
+
+            console.log("1st  : " + (player.y > plats[i].y) + "\n 2nd : " + (player.y + player.height < plats[i].y))
+            if ((player.y > plats[i].y) &&
+                (player.y + player.height < plats[i].y)) {
+                player.y = plats[i].y;
+                player.angle = 0;                                       // To land player on ground without any roation angle
+                onG = true;
+            }
+
+            if ((player.y > plats[i].y) &&
+                (player.y + player.height >= plats[i].y)) {
+                if (player.x < plats[i].x)
+                    if (holdright)
+                        xv = 0;
+
+                if (player.x > plats[i].x)
+                    if (holdleft)
+                        xv = 0;
+            }
+
+
         }
+        plats[i].x -= xv;
+        plats[i].update();
     }
+
+
+
     // Add hovering platforms 
     if (frameNo % 50 == 0) {
-        obstacles.push(new component(100, (canv.height / 2) - Math.floor(Math.random() * (300 - 100) + 100), "#ecf0f1", 100, 10));
+        //obstacles.push(new component(100, (canv.height / 2) - Math.floor(Math.random() * (300 - 100) + 100), "#ecf0f1", 100, 10));
         //obstacles.push(new component(100, (canv.height / 2) - Math.floor(Math.random()*(300 - 100)+100), "black", 3, 3));
     }
 
-    // Add ground platforms
 
 
-    background.update();
+
+
     ctx.fillStyle = "red";
     ctx.fillRect(0, 0, 40, 40);
     ctx.font = "25px";
     ctx.fillStyle = "white";
-    ctx.fillText("X : " + player.x + " Y : " + player.y, canv.width - 200, 40);
-    ctx.fillText("X : " + platform.x + " Y : " + platform.y, canv.width - 200, 80);
-    ctx.fillText("X : " + platform3.x + " Y : " + platform3.y, canv.width - 200, 120);
-    platform.update();
-    platform2.update();
-    platform3.update();
-    //player.newPos();
+    ctx.fillText("Score : " + Math.floor((player.x - (platform.x + 200)) / 15), canv.width - 200, 40);
+
+
     if (!onG) {
         player.angle += 16 * Math.PI / 180;
     }
@@ -86,14 +101,21 @@ function update() {
         obstacles[i].update();
     }
 
+    // Add ground platforms
+    if (plats[plats.length - 1].x + plats[plats.length - 1].width < canv.width - 100)
+        //generatePlatform();
+
+    console.log(plats[plats.length - 1].x, canv.width);
+
 }
 
-function component(x, y, color, width, height) {
+function component(x, y, color, width, height, type) {
     this.width = width;
     this.height = height;
     this.color = color;
     this.x = x;
     this.y = y;
+    this.type = type;
     this.speedX = 0;
     this.speedY = 0;
     this.angle = 0;
@@ -117,8 +139,6 @@ function component(x, y, color, width, height) {
         this.y += this.speedY;
     }
 
-    this.rotate = function () {
-    }
 }
 
 function drawEverything() {
@@ -129,22 +149,33 @@ function drawEverything() {
     background = new component(0, 0, "#3498db", 800, 600);
     background.update();
 
-    platform = new component(0, canv.height / 2, "#ecf0f1", canv.width, canv.height / 2)
+    platform = new component(0, canv.height / 2, "#ecf0f1", canv.width, canv.height / 2, "plane")
     platform.update();
 
-    player = new component(200, 0, "#f1c40f", 20, -20);
+    player = new component(200, 0, "#f1c40f", 20, -20, "plane"); // #e67e22 
     player.update();
 
-    platform2 = new component(canv.width + 100, canv.height / 2, "#ecf0f1", canv.width, canv.height / 2)
+    platform2 = new component(canv.width, (canv.height / 2) - 100, "#ecf0f1", 100, (canv.height / 2) + 100, "last")
     platform2.update();
 
-    platform3 = new component(canv.width, (canv.height / 2) + 100, "#ecf0f1", 100, (canv.height / 2) - 100)
+    platform3 = new component(canv.width + 100, canv.height / 2, "#ecf0f1", canv.width, canv.height / 2, "plane")
     platform3.update();
+
+    lava = new component(-100, (canv.height / 2)+100, "#e74c3c", 100, canv.height / 2, "lava")
+    lava.update();
 
     plats.push(platform);
     plats.push(platform2);
     plats.push(platform3);
+    plats.push(lava);
+    
 
+}
+
+function generatePlatform() {
+    plat = new component(canv.width, canv.height / 2, "#ecf0f1", canv.width, canv.height / 2, "plane")
+    plat.update();
+    plats.push(plat);
 }
 
 function keyDown(event) {
